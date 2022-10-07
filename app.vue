62f135995c2984c31e51714c61d96bd088c8eed6
krishan <krishan@softraw>
2022-10-07 11:15:12 +0530

    counter btn component added

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h2>Hello World</h2>
+    <counter-btn></counter-btn>
   </div>
 </template>
+
+<script></script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ components/CounterBtn.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="counter">
+    <button @click="increament">+</button>
+    <p>{{ count }}</p>
+    <button @click="decreament">-</button>
+  </div>
+</template>
+
+<script setup>
+// composition api====================
+
+// for using com api add attribute in script tag
+
+const count = ref(0);
+
+function increament() {
+  count.value++;
+}
+
+function decreament() {
+  count.value--;
+  console.log(count);
+}
+
+// /option api
+
+// export default {
+//   data() {
+//     return {
+//       count: 0,
+//     };
+//   },
+
+//   methods: {
+//     increament() {
+//       this.count++;
+//     },
+//     decreament() {
+//       this.count--;
+//     },
+//   },
+// };
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  p {
+    font-size: 1rem;
+    color: red;
+    padding: 1rem;
+    font-weight: bold;
+  }
+
+  button {
+    border: 1px solid #333;
+    padding: 0.5rem 1rem;
+    background: black;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: 700;
+    cursor: pointer;
+    transition: all 0.45s ease-in;
+    &:hover {
+      background: rgba(0, 0, 0, 0.8);
+    }
+  }
+}
+</style>
