e75bab680493c54016842d5851fcbf26016754db
krishan <krishan@softraw>
2022-10-07 12:25:25 +0530

    component learning dynamic and normal

--- app.vue
+++ app.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    <h2>Hello World</h2>
-    <counter-btn></counter-btn>
+    <!-- above tag is not like home compnent directly calling it means home is directory and after the component which is welcome or HomeWelcome -->
+
+    <!-- <home-welcome></home-welcome> -->
+
+    <!-- below is direct component-->
+
+    <!-- <counter-btn></counter-btn> -->
+
+    <!-- dynamic component -->
+    <a href="#" @click="view('home')">Home</a>
+    <a href="#" @click="view('counter')">Counter</a>
+
+    <Lazyhome-welcome v-if="page == 'home'"></Lazyhome-welcome>
+    <Lazycounter-btn v-if="page == 'counter'"></Lazycounter-btn>
   </div>
 </template>
 
-<script></script>
+<script setup>
+const page = ref("home");
+
+function view(name) {
+  page.value = name;
+}
+</script>
 
 <style lang="scss" scoped>
 * {
---
+++ components/Home/HomeWelcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Hello Nuxt 3</h1>
+  </div>
+</template>
+
+<script setup>
+// composition api====================
+
+// for using com api add attribute in script tag
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+}
+</style>
